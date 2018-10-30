718d877d24c6ed7513d0bf9cced0d64281cc0cc6
elpresidentenuevo <31987410+elpresidentenuevo@users.noreply.github.com>
2018-10-30 10:02:15 +0100

    FlyingPlayer script

---
+++ Assets/SharedResources/FlyingPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class FlyingPlayer : MonoBehaviour {
+	public float mouseSensitivity = 10
+	;
+ 	public float moveSpeed = 10;
+ 
+	private Vector2 mouseDirection;
+
+	
+	void Update (){
+		mouseDirection += new Vector2(mouseSensitivity * Input.GetAxisRaw("Mouse X"), mouseSensitivity * Input.GetAxisRaw("Mouse Y"));
+		mouseDirection.y = Mathf.Clamp (mouseDirection.y, -90, 90);
+ 
+		transform.localRotation = Quaternion.AngleAxis(mouseDirection.x, Vector3.up);
+		transform.localRotation *= Quaternion.AngleAxis(mouseDirection.y, Vector3.left);
+
+		transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+	}
+}
