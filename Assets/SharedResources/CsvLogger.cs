d92610ccdaf54b1fffa5978036386c61e81603ab
Mikkel Brogaard Vittrup <mikkel.b.vittrup@gmail.com>
2018-11-29 14:32:30 +0100

    Add CsvLogger class

---
+++ Assets/SharedResources/CsvLogger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System;
+
+public class CsvLogger<T> {
+	private Func<T, string> stringParser;
+	private List<T> logItems = new List<T>();
+	private string filePath;
+	private string fileHeader;
+
+	public CsvLogger(string fileName, string _fileHeader, Func<T, string> _stringParser) {
+		stringParser = _stringParser;
+		fileHeader = _fileHeader;
+
+		string date = DateTimeOffset.Now.ToString("s").Replace(':', '_');
+		filePath = fileName + "_" + date + ".csv";
+	}
+
+	public void Log(T item) {
+		logItems.Add(item);
+	}
+
+	public void PrintAndSave() {
+		using(StreamWriter file = new StreamWriter(filePath)) {
+			file.WriteLine(fileHeader);
+			file.Flush();
+
+			foreach(var item in logItems) {
+				var str = stringParser(item);
+				file.WriteLine(str);
+				file.Flush();
+			}
+		}
+	}
+}
