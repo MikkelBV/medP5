42c0ff2d8dfe99ec279e708f4ef523c799f1075d
Mikkel Brogaard Vittrup <mikkel.b.vittrup@gmail.com>
2018-12-05 18:00:25 +0100

    Add logging

--- Assets/4_CaveScene/Scripts/PulseEmitter.cs
+++ Assets/4_CaveScene/Scripts/PulseEmitter.cs
@@ -13,35 +13,53 @@ public class PulseEmitter : MonoBehaviour {
     
 	private PulseVisualizer[] visualisers;
 	private AudioSource audioSource;
-    private bool actionButtonDown = false;
+	private bool actionButtonDown = false;
+
+	private CsvLogger<Vector3> positionLogger = new CsvLogger<Vector3>(true, "path", "x,y,z", vec => vec.x + "," + vec.y + "," + vec.z);
+	private CsvLogger<Vector4> clickLogger = new CsvLogger<Vector4>("clicks", "x,y,z,t", vec => vec.x + "," + vec.y + "," + vec.z + "," + vec.w);
 
 	void Start () {
 		audioSource = GetComponent<AudioSource>();
 		visualisers = Object.FindObjectsOfType<PulseVisualizer>();
-		distance = maxDistance;
+		distance = maxDistance; 
+
+		positionLogger.StartAsyncLogging();
+	}
+
+	void OnApplicationQuit() {
+		positionLogger.PrintAndSave();
+		clickLogger.PrintAndSave();
 	}
 
 	void Update () {
-        OVRInput.Update();
+		OVRInput.Update();
 		distance += speed * Time.deltaTime;
 
-        bool buttonPressed;
-		if (isVR) buttonPressed = OVRInput.Get(OVRInput.Button.One);
-		else buttonPressed = Input.GetMouseButtonDown(0);
+		bool buttonPressed;
 
-        if (buttonPressed && !actionButtonDown) {
-			if (distance > maxDistance) {
-            	EmitSound(100f, 8f);
-			}
-            audioSource.Play();
-        }
+		if (isVR) {
+			buttonPressed = OVRInput.Get(OVRInput.Button.One);
+		} else {
+			buttonPressed = Input.GetMouseButtonDown(0);
+		}
 
-        actionButtonDown = buttonPressed;
-        RayCasting();
+		if (buttonPressed && !actionButtonDown && distance > maxDistance) {
+				EmitSound(100f, 8f);
+				audioSource.Play();
+		}
+
+		actionButtonDown = buttonPressed;
+		RayCasting();
+		positionLogger.Log(transform.position);
 	}
 
 	void EmitSound(float freq, float intensity) {
-		var pulseOrigin = new Vector4(transform.position.x, transform.position.y, transform.position.z, 0);
+		var pulseOrigin = new Vector4(
+			transform.position.x, 
+			transform.position.y, 
+			transform.position.z, 
+			1
+		);
 
 		foreach (var visualizer in visualisers) {
 			visualizer.thisRenderer.material.SetVector("_Origin", pulseOrigin);
@@ -50,18 +68,19 @@ public class PulseEmitter : MonoBehaviour {
 		}
 
 		distance = 0.0f;
+
+		pulseOrigin.w = Time.realtimeSinceStartup;
+		clickLogger.Log(pulseOrigin);
 	}
 
 	void RayCasting () {
-		RaycastHit hit; 
+		RaycastHit collision; 
 		float raySum = 0;
 
 		foreach (var ray in rays){
-			if(Physics.Raycast(transform.position, ray, out hit)){
-				var rayDistance = hit.distance;
-				raySum += rayDistance;
-
-				Debug.DrawRay(transform.position, ray * rayDistance, Color.green);
+			if(Physics.Raycast(transform.position, ray, out collision)) {
+				raySum += collision.distance;
+				Debug.DrawRay(transform.position, ray * collision.distance, Color.green);
 			} else {
 				Debug.DrawRay(transform.position, ray * 10, Color.red);
 			}
--- Assets/SharedResources/CsvLogger.cs
+++ Assets/SharedResources/CsvLogger.cs
@@ -3,26 +3,47 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System;
+using System.Threading;
+using UnityEngine;
 
 public class CsvLogger<T> {
 	private Func<T, string> stringParser;
-	private List<T> logItems = new List<T>();
+	private Queue<T> logItems = new Queue<T>();
 	private string filePath;
 	private string fileHeader;
 
-	public CsvLogger(string fileName, string _fileHeader, Func<T, string> _stringParser) {
+	private bool logAsync = false;
+	private volatile bool shouldLog = false;
+	private Thread datalogger;
+
+	public CsvLogger(string fileName, string _fileHeader, Func<T, string> _stringParser) 
+	: this(false, fileName, _fileHeader, _stringParser) { }
+
+	public CsvLogger(bool _logAsync, string fileName, string _fileHeader, Func<T, string> _stringParser) {
 		stringParser = _stringParser;
 		fileHeader = _fileHeader;
 
 		string date = DateTimeOffset.Now.ToString("s").Replace(':', '_');
 		filePath = fileName + "_" + date + ".csv";
+
+		if (_logAsync) {
+			logAsync = true;
+			shouldLog = true;
+		} else {
+			logAsync = false;
+		}
 	}
 
 	public void Log(T item) {
-		logItems.Add(item);
+		logItems.Enqueue(item);
 	}
 
 	public void PrintAndSave() {
+		if (logAsync) {
+			shouldLog = false;
+			return;
+		}
+
 		using(StreamWriter file = new StreamWriter(filePath)) {
 			file.WriteLine(fileHeader);
 			file.Flush();
@@ -31,6 +52,27 @@ public class CsvLogger<T> {
 				var str = stringParser(item);
 				file.WriteLine(str);
 				file.Flush();
+			}
+		}
+	}
+
+	public void StartAsyncLogging() {
+		datalogger = new Thread(LogToFileAsync);
+		datalogger.Start();
+	}
+
+	private void LogToFileAsync() {
+		using(StreamWriter file = new StreamWriter(filePath)) {
+			file.WriteLine(fileHeader);
+			file.Flush();
+
+			while(shouldLog) {
+				if (logItems.Count > 0) {
+					var item = logItems.Dequeue();
+					var str = stringParser(item);
+					file.WriteLine(str);
+					file.Flush();
+				}
 			}
 		}
 	}
