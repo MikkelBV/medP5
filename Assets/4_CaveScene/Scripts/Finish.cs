fea887594c2e953360897a8c2a145f44fe4f199e
Mikkel Brogaard Vittrup <mikkel.b.vittrup@gmail.com>
2018-12-08 10:14:50 +0100

    Log completion time

---
+++ Assets/4_CaveScene/Scripts/Finish.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour {
+	private CsvLogger<float> logger = new CsvLogger<float>("completion-time", "t", f => f.ToString()); 
+
+	void OnTriggerEnter(Collider collider) {
+		print(Time.realtimeSinceStartup);
+		logger.Log(Time.realtimeSinceStartup);
+		logger.PrintAndSave();
+	}
+}
