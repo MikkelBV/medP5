4614a66f9ad9616171ba10961496d9c8c2341a44
David Eriksen <deriks16@student.aau.dk>
2018-12-10 23:15:05 +0100

    vrplayer spam fix

--- Assets/SharedResources/VRPlayer.cs
+++ Assets/SharedResources/VRPlayer.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class VRPlayer : MonoBehaviour {
 	private CharacterController controller;
 	private CsvLogger<Vector4> collisionLogger = new CsvLogger<Vector4>("collisions", "x,y,z,t", vec => vec.x + "," + vec.y + "," + vec.z + "," + vec.w);
+    private float timer;
 
 	void Start () {
 		controller = GetComponent<CharacterController>();
+        timer = 2.0f;
 	}
 
 	void OnApplicationQuit() {
@@ -16,17 +18,24 @@ public class VRPlayer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if ((controller.collisionFlags & CollisionFlags.Sides) != 0){
-				Debug.Log("Collide with wall.");
-				
-				Vector4 point = new Vector4(
-					transform.position.x,
-					transform.position.y,
-					transform.position.z,
-					Time.time
-				);
+        timer -= Time.deltaTime;
 
-				collisionLogger.Log(point);
-		}
+        if (timer < 0)
+        {
+            if ((controller.collisionFlags & CollisionFlags.Sides) != 0)
+            {
+                timer = 2.0f;
+                Debug.Log("Collide with wall.");
+
+                Vector4 point = new Vector4(
+                    transform.position.x,
+                    transform.position.y,
+                    transform.position.z,
+                    Time.time
+                    );
+
+                collisionLogger.Log(point);
+            }
+        }
 	}
 }
